4cfb7b4a8584989964a367e1b13ecd6a63f2a444
Leonardo Muller <leonardomuller78@gmail.com>
2021-04-04 21:04:26 -0300

    dia 04/04

---
+++ aulas03/selecionar o dom/script.js
@@ -0,0 +1,19 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl= document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInternos = document.querySelector("[href^='#'")
+console.log(linkInternos);
+
+const animaisImg = document.querySelectorAll(".animais img");
+
+console.log(animaisImg);
